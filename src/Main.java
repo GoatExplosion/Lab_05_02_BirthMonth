bc6b97a059b9275ed02985f5b6a55169a497b00d
nathangoodlett <N.E.Goodlett4@gmail.com>
2022-09-22 15:01:11 -0400

    Attempt 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+      int birthMonth = 12;
+
+      if(birthMonth <= 12)
+      {
+          System.out.println("Your birth month is " + birthMonth);
+      }
+      else
+      {
+          System.out.println("Error: You entered an invalid month");
+      }
+
     }
 }
